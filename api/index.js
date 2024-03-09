2490febda39c0593d912c5e57346b7a49f852829
Non00010110000 <nonhussien5@gmail.com>
2024-03-09 10:34:40 +0300

    connected to database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,10 @@
 import express from "express";
+import mongoose from "mongoose";
 
 const app = express();
-
+mongoose.connect("mongodb://127.0.0.1:27017/real-state").then(() => {
+  console.log("connetced to mongo");
+});
 app.listen(3000, () => {
   console.log("server is running on port 3000");
 });
